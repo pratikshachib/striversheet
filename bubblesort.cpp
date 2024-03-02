53b7ef0ccf524f71f878ba6e4eab6a4bec8527f1
Pratiksha chib <155293232+pratikshachib@users.noreply.github.com>
2024-03-02 20:46:50 +0530

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[], int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "After sorting: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    int arr[] = {23, 45, 87, 12, 20, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Sorting: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubble_sort(arr, n);
+    return 0;
+
+}
